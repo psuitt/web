16f66a083f0747fbe35898ef66cdd1dab82bb5f7
Phillip Suitt <suitt.phillip@live.com>
2014-03-06 23:23:42 -0700

    Icons

--- nofoods/client/food/add/add.js
+++ nofoods/client/food/add/add.js
@@ -1,17 +1,21 @@
 (function() {
-	Template.foodsAdd.events({
-		'click .rating': function (event, template) {
-			if (template.find(".rating.selected")) {
-				template.find(".rating.selected").className = "rating";
-			}
-			event.target.className = "rating selected";
-		},
+	Template.foodsAdd.rendered = function() {
+		$('#ratingDiv span.rating').on('click', function() {
+			var index = $(this).index();			
+			$('#ratingDiv span.rating').each(function() {
+				$(this).toggleClass('full', $(this).index() <= index);						
+			});		
+		});	
 
+	};
+	
+	Template.foodsAdd.events({
 		'click #save': function (event, template) {
 			var type = $("input[name='type']:checked").val(); 
 			var name = template.find("#name").value;
 			var brand = template.find("#brand").value;
-			var rating = parseInt(template.find(".selected").innerHTML, 10); 
+			var last = $('#ratingDiv span.rating.full').last();
+			var rating = parseInt((last.index() + 1), 10); 
 		
 			var id = createFood({
 				rating: rating,
@@ -29,5 +33,8 @@
 		}
 
 	});
+
+
+
 })();
 
