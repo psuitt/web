46d78dbd18710e378b2da7856283363cdc29bf41
Phillip Suitt <suitt.phillip@live.com>
2014-03-17 17:42:46 -0600

    Updates to fix saving

--- nofoods/server/validation.js
+++ nofoods/server/validation.js
@@ -1,6 +1,11 @@
 Meteor.methods({
 
 	validate: function(words) {
+		check(words, Array);
+
+		for (var i = 0, l = words.length; i < l ; i += 1)
+			check(words[i], String);
+
 		if (Words.findOne( { word: { $in: words } } )) {
 			throw new Meteor.Error(400, "Invalid text");
 		}
