59c8ea5d50cb4ccd5754526b954af8af5868591c
Phillip Suitt <suitt.phillip@live.com>
2014-04-01 22:12:25 -0600

    Popular map started works based on location.

--- nofoods/client/popular/popular.js
+++ nofoods/client/popular/popular.js
@@ -1,3 +1,5 @@
+MAP_DATA = {};
+
 Template.popular.rendered = function() {
 	
 	Meteor.subscribe('statistics_country', function() {
@@ -8,8 +10,48 @@ Template.popular.rendered = function() {
 			div.html(stat.country + " Foods and Drinks: " + stat.length);
 			$("div.contents").append(div);
 			
+			MAP_DATA[stat.countrycode] = {
+				numberofitems: stat.length		
+			};
+			
 		});
 		
 	});
+	
+	initMap();
+		
+};
 
+var initMap = function () {
+	
+	$("#popular-worldmap").vectorMap({
+		map: 'world_en',
+		backgroundColor: '#a5bfdd',
+		borderColor: '#818181',
+		borderOpacity: 0.25,
+		borderWidth: 1,
+		color: '#f4f3f0',
+		enableZoom: true,
+		hoverColor: '#c9dfaf',
+		hoverOpacity: null,
+		normalizeFunction: 'linear',
+		scaleColors: ['#b6d6ff', '#005ace'],
+		selectedColor: '#c9dfaf',
+		selectedRegion: null,
+		showTooltip: true,
+	  onLabelShow: function(element, label, code) {
+	  	if (MAP_DATA[code.toUpperCase()]) {
+	  		label.append("</br>Foods and Drinks: " + MAP_DATA[code.toUpperCase()].numberofitems);
+	  	}
+	  },
+		onRegionClick: function(element, code, region) {
+			var message = 'You clicked "'
+	            + region 
+	            + '" which has the code: '
+	            + code.toUpperCase();
+             
+	        alert(message);
+	  }
+	});
+	
 };
