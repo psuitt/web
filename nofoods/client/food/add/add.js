8ee35f63866c0c57ab9143ee581def5644e3a55c
Phillip Suitt <suitt.phillip@live.com>
2014-04-15 21:59:45 -0600

    Added map to the ad page for lookup

--- nofoods/client/food/add/add.js
+++ nofoods/client/food/add/add.js
@@ -1,6 +1,7 @@
 (function() {
 	
-	var place;
+	var placeauto,
+			map;
 	
 	Template.foodsAdd.rendered = function() {
 		$('div.ratingDiv span.rating').on('click', function() {
@@ -22,17 +23,25 @@
   		bounds: defaultBounds
 		};
 	
-		place = new google.maps.places.Autocomplete(input, options);	
+		placeauto = new google.maps.places.Autocomplete(input, options);	
 		
 		if (navigator.geolocation) {
    		navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = new google.maps.LatLng(
-          position.coords.latitude, position.coords.longitude);
-      place.setBounds(new google.maps.LatLngBounds(geolocation,
-          geolocation));
-    });
-  }
-
+      	var geolocation = new google.maps.LatLng(
+          	position.coords.latitude, position.coords.longitude);
+      	placeauto.setBounds(new google.maps.LatLngBounds(geolocation,
+          	geolocation));
+          	
+      	map = new google.maps.Map(document.getElementById('foodsadd-map'), {
+					center: geolocation,
+					zoom: 13    	
+      	});	
+    	});
+  	} else {
+			map = new google.maps.Map(document.getElementById('foodsadd-map'));  	
+  	}
+  
+  	setUpMap();
 
 	};
 	
@@ -96,7 +105,7 @@
 
 	var setPlaceDetails = function(data) {
 		
-		var placeDetails = place.getPlace();
+		var placeDetails = placeauto.getPlace();
 		
 		if (placeDetails) {
 		
@@ -124,6 +133,56 @@
 			}			
 					
 		}		
+	
+	};
+	
+	var setUpMap = function() {
+		
+		var infowindow = new google.maps.InfoWindow();
+  	var marker = new google.maps.Marker({
+   		map: map,
+    	anchorPoint: new google.maps.Point(0, -29)
+  	});
+
+		
+		google.maps.event.addListener(placeauto, 'place_changed', function() {
+    	infowindow.close();
+    	marker.setVisible(false);
+    	var place = placeauto.getPlace();
+    	if (!place.geometry) {
+    	  return;
+   	 }
+
+   	 // If the place has a geometry, then present it on a map.
+  	  if (place.geometry.viewport) {
+ 	     map.fitBounds(place.geometry.viewport);
+ 	   } else {
+  	    map.setCenter(place.geometry.location);
+ 	     map.setZoom(17);  // Why 17? Because it looks good.
+ 	   }
+  	  marker.setIcon(/** @type {google.maps.Icon} */({
+  	    url: place.icon,
+      	size: new google.maps.Size(71, 71),
+      	origin: new google.maps.Point(0, 0),
+      	anchor: new google.maps.Point(17, 34),
+      	scaledSize: new google.maps.Size(35, 35)
+   	 }));
+   	 marker.setPosition(place.geometry.location);
+   	 marker.setVisible(true);
+
+    	var address = '';
+    	if (place.address_components) {
+      	address = [
+        	(place.address_components[0] && place.address_components[0].short_name || ''),
+        	(place.address_components[1] && place.address_components[1].short_name || ''),
+        	(place.address_components[2] && place.address_components[2].short_name || '')
+      	].join(' ');
+    	}
+
+    	infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    	infowindow.open(map, marker);
+  	});
+
 	
 	};
 
