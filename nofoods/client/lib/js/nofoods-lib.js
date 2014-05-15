ee621a8b86117dee22ae2296170f721cefa6ea8e
Phillip Suitt <suitt.phillip@live.com>
2014-05-14 23:31:26 -0600

    Finished the paging div

--- nofoods/client/lib/js/nofoods-lib.js
+++ nofoods/client/lib/js/nofoods-lib.js
@@ -20,12 +20,15 @@ NoFoods.lib = function() {
 				return "";	
 			
 			var div = $("<div class='myfoods-paging pagingdiv'></div>"),
-					leftArrow = $("<span class='leftarrow'>&#60;</span>"),
-					rightArrow = $("<span class='rightarrow'>&#62;</span>")
-					lastPage = MAX_PAGE_AMOUNT;
+					firstArrow = $("<span class='first arrow'>&#60;&#60;</span>"),
+					leftArrow = $("<span class='left arrow'>&#60;</span>"),
+					rightArrow = $("<span class='right arrow'>&#62;</span>"),
+					lastArrow = $("<span class='last arrow'>&#62;&#62;</span>"),
+					lastPage = MAX_PAGE_AMOUNT,
+					maxVal= Math.round(max);
 					
-			if (max < lastPage) 
-				lastPage = max;
+			if (maxVal < lastPage) 
+				lastPage = maxVal;
 			
 			for (var i = 1; i <= lastPage; i += 1 ) {
 				var span = $("<span class='paging'>" + i + "</span>");
@@ -36,35 +39,85 @@ NoFoods.lib = function() {
 				div.append(span);
 			}
 			
-			div.attr("max", max);
-			
-			if (callback)
-				div.on('click', 'span.paging', function() {
-					div.find('span.current').removeClass('current'); 
-					$(this).addClass('current');
-					callback(parseInt($(this).html(), 10));			
-				});
+			div.attr("max", maxVal);
 			
 			leftArrow.bind("click", function() {
 				var first = div.find(".paging").first(),
-						firstVal = parseInt(first.html(), 10);			
+						firstVal = parseInt(first.html(), 10);
+		
 				if (firstVal > 1) {
 					div.find(".paging").last().remove();
-					$(this).after($("<span class='paging'>" + (firstVal - 1) + "</span>"))
+					$(this).after($("<span class='paging'>" + (firstVal - 1) + "</span>"));
 				}				
 			});	
 			
 			rightArrow.bind("click", function() {
+				
 				var last = div.find(".paging").last(),
-						lastVal = parseInt(last.html(), 10);			
-				if (max > lastVal) {
+						lastVal = parseInt(last.html(), 10),
+						maxVal = parseInt(div.attr("max"), 10);	
+									
+				if (maxVal > lastVal) {
 					div.find(".paging").first().remove();
-					$(this).before($("<span class='paging'>" + (lastVal + 1) + "</span>"))
+					$(this).before($("<span class='paging'>" + (lastVal + 1) + "</span>"));
 				}		
 			});				
 			
 			div.prepend(leftArrow);
 			div.append(rightArrow);
+			
+			if (callback) {
+				
+				div.on('click', 'span.paging', function() {
+					div.find('span.current').removeClass('current'); 
+					$(this).addClass('current');
+					callback(parseInt($(this).html(), 10));			
+				});
+				
+				firstArrow.bind("click", function() {
+					
+					div.find(".paging").remove();	
+							
+					for (var i = 1; i <= lastPage; i += 1 ) {
+						var span = $("<span class='paging'>" + i + "</span>");
+						
+						if (i === 1) 
+							span.addClass('current');		
+						
+						rightArrow.before(span);
+					}
+					
+					callback(1);	
+								
+				});	
+				
+				lastArrow.bind("click", function() {
+					
+					var maxVal = parseInt(div.attr("max"), 10),
+							v = maxVal - MAX_PAGE_AMOUNT;
+					
+					if (v < 0)
+						v = 0;	
+					
+					div.find(".paging").remove();	
+							
+					for (var i = maxVal; i > v; i -= 1 ) {
+						var span = $("<span class='paging'>" + i + "</span>");
+						
+						if (i === maxVal) 
+							span.addClass('current');		
+						
+						leftArrow.after(span);
+					}
+					
+					callback(maxVal);
+					
+				});
+				
+				div.prepend(firstArrow);
+				div.append(lastArrow);	
+				
+			}		
 			
 			return div;
 				
