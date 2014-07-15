e043ccb87e9f306cce8ce59e779c7ae0e7976d83
Phillip Suitt <suitt.phillip@live.com>
2014-07-14 21:43:44 -0600

    Achievments list

--- nofoods/server/achievements/achievements-rating.js
+++ nofoods/server/achievements/achievements-rating.js
@@ -84,14 +84,39 @@ UPDATE_METHODS['RATING'] = function(code) {
 /**
  * Increment when any new ratings occur.
  */
-AddAchievement('COUNT_A', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+AddAchievement('COUNT_A', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [ 
 	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
-	{cap: 2, title: 'Novice Rater' },
-	{cap: 4, title: 'Rate your first item 3' },
-	{cap: 10, title: 'Rate your first item 4' },
-	{cap: 5, title: 'Rate your first item 5' },
-	{cap: 6, title: 'Rate your first item 6' },
-	{cap: 9001, title: 'Rate your first item' }
+	{cap: 5, title: 'Novice Rater' },
+	{cap: 10, title: 'Rate your first item 3' },
+	{cap: 25, title: 'Rate your first item 4' },
+	{cap: 50, title: 'Rate your first item 5' },
+	{cap: 75, title: 'Rate your first item 5' },
+	{cap: 100, title: 'Rate your first item 6' },
+	{cap: 150, title: 'Rate your first item' },
+	{cap: 200, title: 'Rate your first item' },
+	{cap: 250, title: 'Rate your first item' },
+	{cap: 300, title: 'Rate your first item' },
+	{cap: 350, title: 'Rate your first item' },
+	{cap: 400, title: 'Rate your first item' },
+	{cap: 450, title: 'Rate your first item' },
+	{cap: 500, title: 'Rate your first item' },
+	{cap: 750, title: 'Rate your first item' },
+	{cap: 1000, title: 'Rate your first item' },
+	{cap: 1250, title: 'Rate your first item' },
+	{cap: 1500, title: 'Rate your first item' },
+	{cap: 2000, title: 'Rate your first item' },
+	{cap: 2500, title: 'Rate your first item' },
+	{cap: 5000, title: 'Rate your first item' },
+	{cap: 7500, title: 'Rate your first item' },
+	{cap: 10000, title: 'Rate your first item' },
+	{cap: 15000, title: 'Rate your first item' },
+	{cap: 20000, title: 'Rate your first item' },
+	{cap: 25000, title: 'Rate your first item' },
+	{cap: 30000, title: 'Rate your first item' },
+	{cap: 35000, title: 'Rate your first item' },
+	{cap: 40000, title: 'Rate your first item' },
+	{cap: 45000, title: 'Rate your first item' },
+	{cap: 50000, title: 'Rate your first item', difficulty: 1000 }
 ]);
 
 /**
@@ -111,6 +136,97 @@ AddAchievement('COUNT_F', "Rate Something", 'Rate %cap different food items.', '
  * Increment when any new ratings occur on drinks.
  */
 AddAchievement('COUNT_D', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
+	{cap: 2, title: 'Novice Rater' },
+	{cap: 4, title: 'Rate your first item 3' },
+	{cap: 10, title: 'Rate your first item 4' },
+	{cap: 5, title: 'Rate your first item 5' },
+	{cap: 6, title: 'Rate your first item 6' },
+	{cap: 9001, title: 'Rate your first item' }
+]);
+
+/**
+ * Increment when any new ratings occur on Sunday.
+ */
+AddAchievement('COUNT_SUN', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
+	{cap: 2, title: 'Novice Rater' },
+	{cap: 4, title: 'Rate your first item 3' },
+	{cap: 10, title: 'Rate your first item 4' },
+	{cap: 5, title: 'Rate your first item 5' },
+	{cap: 6, title: 'Rate your first item 6' },
+	{cap: 9001, title: 'Rate your first item' }
+]);
+
+/**
+ * Increment when any new ratings occur on Monday.
+ */
+AddAchievement('COUNT_MON', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
+	{cap: 2, title: 'Novice Rater' },
+	{cap: 4, title: 'Rate your first item 3' },
+	{cap: 10, title: 'Rate your first item 4' },
+	{cap: 5, title: 'Rate your first item 5' },
+	{cap: 6, title: 'Rate your first item 6' },
+	{cap: 9001, title: 'Rate your first item' }
+]);
+
+/**
+ * Increment when any new ratings occur on Tuesday.
+ */
+AddAchievement('COUNT_TUE', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
+	{cap: 2, title: 'Novice Rater' },
+	{cap: 4, title: 'Rate your first item 3' },
+	{cap: 10, title: 'Rate your first item 4' },
+	{cap: 5, title: 'Rate your first item 5' },
+	{cap: 6, title: 'Rate your first item 6' },
+	{cap: 9001, title: 'Rate your first item' }
+]);
+
+/**
+ * Increment when any new ratings occur on Wednesday.
+ */
+AddAchievement('COUNT_WED', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
+	{cap: 2, title: 'Novice Rater' },
+	{cap: 4, title: 'Rate your first item 3' },
+	{cap: 10, title: 'Rate your first item 4' },
+	{cap: 5, title: 'Rate your first item 5' },
+	{cap: 6, title: 'Rate your first item 6' },
+	{cap: 9001, title: 'Rate your first item' }
+]);
+
+/**
+ * Increment when any new ratings occur on Thursday.
+ */
+AddAchievement('COUNT_THU', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
+	{cap: 2, title: 'Novice Rater' },
+	{cap: 4, title: 'Rate your first item 3' },
+	{cap: 10, title: 'Rate your first item 4' },
+	{cap: 5, title: 'Rate your first item 5' },
+	{cap: 6, title: 'Rate your first item 6' },
+	{cap: 9001, title: 'Rate your first item' }
+]);
+
+/**
+ * Increment when any new ratings occur on Friday.
+ */
+AddAchievement('COUNT_FRI', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
+	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
+	{cap: 2, title: 'Novice Rater' },
+	{cap: 4, title: 'Rate your first item 3' },
+	{cap: 10, title: 'Rate your first item 4' },
+	{cap: 5, title: 'Rate your first item 5' },
+	{cap: 6, title: 'Rate your first item 6' },
+	{cap: 9001, title: 'Rate your first item' }
+]);
+
+/**
+ * Increment when any new ratings occur on Saturday.
+ */
+AddAchievement('COUNT_SAT', "Rate Something", 'Rate %cap different food items.', 'RATING', 1, [
 	{cap: 1, title: 'Rate your first item', description: 'Rate a single food item.'},
 	{cap: 2, title: 'Novice Rater' },
 	{cap: 4, title: 'Rate your first item 3' },
