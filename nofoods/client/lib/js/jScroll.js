de67d6754330054355d50b360fae1a812804affd
Phillip Suitt <suitt.phillip@live.com>
2014-09-09 23:12:53 -0600

    Started on mobile version and reported screen.

--- nofoods/client/lib/js/jScroll.js
+++ nofoods/client/lib/js/jScroll.js
@@ -51,7 +51,7 @@
 		// Private 
 		function location($element)
 		{
-			this.min = $element.offset().top - 50;
+			this.min = $element.offset().top - 57;
 			this.originalMargin = parseInt($element.css("margin-top"), 10) || 0;
 			
 			this.getMargin = function ($window)
